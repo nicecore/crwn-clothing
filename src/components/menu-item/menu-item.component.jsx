8ba2b5fdcedf0e3ca84c63d8ab24d01473897663
Nicecore <adamdcameron@gmail.com>
2022-02-23 10:52:54 -0600

    updated lots of css

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,9 +1,15 @@
 import React from "react";
+import './menu-item.styles.scss';
 
-const MenuItem = ({title}) => (
-    <div className="menu-item">
+const MenuItem = ({title, imageUrl, size}) => (
+    <div className={`${size} menu-item`}>
+        {/* Standalone one-tag div */}
+        <div className="background-image" style={{
+        // Use JS template string to dynamically set a style rule on an HTML element
+        backgroundImage: `url(${imageUrl})`
+        }}/>
         <div className='content'>
-            <h1 className='title'>{title}</h1>
+            <h1 className='title'>{title.toUpperCase()}</h1>
             <span className='subtitle'>SHOP NOW</span>
         </div>
     </div>
