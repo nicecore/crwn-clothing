0edcadc8da6b41fd0b062a867f23c5462f23b553
Nicecore <adamdcameron@gmail.com>
2022-02-24 11:32:42 -0600

    update menu items to navigate withRouter HOC

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,19 +1,23 @@
 import React from "react";
-import './menu-item.styles.scss';
+import "./menu-item.styles.scss";
+import { withRouter } from "react-router-dom";
 
-const MenuItem = ({title, imageUrl, size}) => (
-    <div className={`${size} menu-item`}>
-        {/* Standalone one-tag div */}
-        <div className="background-image" style={{
+const MenuItem = ({ title, imageUrl, size, history, linkUrl, match }) => (
+  <div className={`${size} menu-item`} 
+    onClick={() => history.push(`${match.url}${linkUrl}`)}>
+    {/* Standalone one-tag div */}
+    <div
+      className="background-image"
+      style={{
         // Use JS template string to dynamically set a style rule on an HTML element
-        backgroundImage: `url(${imageUrl})`
-        }}/>
-        <div className='content'>
-            <h1 className='title'>{title.toUpperCase()}</h1>
-            <span className='subtitle'>SHOP NOW</span>
-        </div>
+        backgroundImage: `url(${imageUrl})`,
+      }}
+    />
+    <div className="content">
+      <h1 className="title">{title.toUpperCase()}</h1>
+      <span className="subtitle">SHOP NOW</span>
     </div>
-)
+  </div>
+);
 
-
-export default MenuItem;
+export default withRouter(MenuItem);
--- src/index.js
+++ src/index.js
@@ -2,12 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import * as serviceWorkerRegistration from './serviceWorkerRegistration';
-import reportWebVitals from './reportWebVitals';
+import {BrowserRouter} from 'react-router-dom';
 
 ReactDOM.render(
-  <React.StrictMode>
+  // Wrapping App component in BrowserRouter component gives it routing functionality.
+  <BrowserRouter>
     <App />
-  </React.StrictMode>,
+  </BrowserRouter>,
   document.getElementById('root')
 );
