7d1ce2570c32ce24738e4887755b1b6e5a32f8cc
Nicecore <adamdcameron@gmail.com>
2022-03-11 15:35:44 -0600

    cart dropdown

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -4,12 +4,16 @@ import { Link } from 'react-router-dom';
 import { ReactComponent as Logo } from '../../assets/crown.svg';
 import { auth } from '../../firebase/firebase.utils'; 
 import { connect } from 'react-redux';
+import CartIcon from "../cart-icon/cart-icon.component";
 
-const Header = ({ currentUser }) => (
+import CartDropdown from "../cart-dropdown/cart-dropdown.component";
+
+const Header = ({ currentUser, hidden }) => (
     <div className="header">
         <Link className="logo-container" to="/">
             <Logo className="logo" />
         </Link>
+
         <div className="options">
             <Link to="/shop" className="option">
                 SHOP
@@ -23,12 +27,19 @@ const Header = ({ currentUser }) => (
                 :
                 <Link className="option" to="/signin">SIGN IN</Link>
             }
+            <CartIcon></CartIcon>
         </div>
+        {
+            hidden ? null :
+            <CartDropdown />
+        }
+        
     </div>
 );
 
-const mapStateToProps = state => ({
-    currentUser: state.user.currentUser
+const mapStateToProps = ({user: { currentUser }, cart: { hidden }}) => ({
+    currentUser,
+    hidden
 })
 
 export default connect(mapStateToProps)(Header);
